e0bfa4c535ac5fc6ce6e92053f0134900f28b782
Richard Wise <richard.wise@hey.com>
2023-12-05 10:53:22 -0500

    password sharing roles and permissions

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,6 +1,8 @@
 class PasswordsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_password, except: [:index, :new, :create]
+  before_action :require_editable_permission, only: [:edit, :update]
+  before_action :require_deletable_permission, only: [:destroy]
 
   def index
     @passwords = current_user.passwords
@@ -13,9 +15,10 @@ class PasswordsController < ApplicationController
   end
 
   def create
-    @password = current_user.passwords.create(password_params) # Using create here to ensure the join record and child record are made
+    @password = Password.new(password_params)
+    @password.user_passwords.new(user: current_user, role: :owner)
 
-    if @password.persisted? # User persisted insated of save due to using create above
+    if @password.save
       redirect_to @password
     else
       render :new, status: :unprocessable_entity
@@ -45,5 +48,14 @@ class PasswordsController < ApplicationController
 
   def set_password
     @password = current_user.passwords.find(params[:id])
+
+  end
+
+  def require_editable_permission
+    redirect_to @password unless current_user_password.editable?
+  end
+
+  def require_deletable_permission
+    redirect_to @password unless current_user_password.deletable?
   end
 end
